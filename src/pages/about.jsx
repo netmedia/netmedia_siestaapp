5b98e96bf54940046cb7e6746a74bbcd835782e0
Valentina Tucak <vtucak@Valentinas-MacBook-Pro.local>
2022-02-28 15:50:03 +0100

    About changes

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,12 +1,27 @@
 import React from "react";
 import Sidebar from "../components/sidebar/sidebar";
+import { Col, Container, Row } from "react-bootstrap";
 
 function About() {
   return (
-    <div>
-      <Sidebar />
-      About
-    </div>
+    <Container fluid>
+      <Row>
+        <Col>
+          <Sidebar />
+        </Col>
+        <Col className="ml-[350px] pt-10 text-3xl block">
+          <p className="animate-bounce py-10 bounceIn">Better sleep</p>
+          <p className="animate-bounce flex justify-end pr-20 py-10">
+            Better you
+          </p>
+          <p className="animate-bounce py-10">Track your sleep cycle</p>
+          <p className="animate-bounce flex justify-end pr-10 py-10">
+            Monitor sleep quality
+          </p>
+          <p className="animate-bounce py-10">Improve your daily life</p>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
