02c5b9eee838aa73d6d836476562b69c07165d92
Valentina Tucak <vtucak@Valentinas-MacBook-Pro.local>
2022-03-01 10:35:13 +0100

    About changes

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -9,7 +9,13 @@ function About() {
         <Col>
           <Sidebar />
         </Col>
-        <Col className="ml-[350px] pt-10 text-3xl block">
+        <Col className="ml-[350px] 2xl:pt-20 text-3xl block">
+          <Row className="">
+            <h2 className="font-normal text-xl px-5">About</h2>
+            <p className="font-light text-gray-400 text-sm px-5 py-2">
+              Home / About
+            </p>
+          </Row>
           <p className="animate-bounce py-10 bounceIn">Better sleep</p>
           <p className="animate-bounce flex justify-end pr-20 py-10">
             Better you
