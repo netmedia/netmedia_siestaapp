de033964d3273bf461b91fdd448979543410dc7a
Valentina Tucak <vtucak@Valentinas-MacBook-Pro.local>
2022-03-02 15:19:26 +0100

    Fixing about

--- src/components/entries/entryList.jsx
+++ src/components/entries/entryList.jsx
@@ -6,7 +6,13 @@ const EntryList = ({ setShowForm }) => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(getAllEntries("http://localhost:3005/entries"));
+    dispatch(
+      getAllEntries(
+        `http://localhost:3005/entries?userId=${
+          JSON.parse(localStorage.getItem("loginData")).googleId
+        }`
+      )
+    );
     // eslint-disable-next-line
   }, []);
 
@@ -21,25 +27,30 @@ const EntryList = ({ setShowForm }) => {
   };
 
   return (
-    <div className='flex flex-wrap gap-5 p-5'>
+    <div className="flex flex-wrap gap-5 p-5">
       {listOfEntries.map((value, key) => {
         return (
-          <div key={key} className='flex'>
-            <div className='flex flex-col gap-8 place-items-start justify-between bg-siesta-grey-light px-4 py-4 text-sm rounded-xl w-[250px]'>
+          <div key={key} className="flex">
+            <div className="flex flex-col gap-8 place-items-start justify-between bg-siesta-grey-light px-4 py-4 text-sm rounded-xl w-[250px]">
               <div>
-                <p className='text-siesta-blue-light font-bold'>{value.title}</p>
+                <p className="text-siesta-blue-light font-bold">
+                  {value.title}
+                </p>
               </div>
               <div>
-                <p className='font-bold text-siesta-grey-dark'>{formatDate(value.date)}</p>
+                <p className="font-bold text-siesta-grey-dark">
+                  {formatDate(value.date)}
+                </p>
               </div>
               <div>
-                <p className='font-bold text-siesta-grey-dark'>
+                <p className="font-bold text-siesta-grey-dark">
                   {value.startTime} - {value.endTime}
                 </p>
               </div>
               <div>
-                <p className='font-bold text-red-600'>
-                  {parseInt(value.endTime) - parseInt(value.startTime)} hours of sleep
+                <p className="font-bold text-red-600">
+                  {parseInt(value.endTime) - parseInt(value.startTime)} hours of
+                  sleep
                 </p>
               </div>
             </div>
--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -9,22 +9,30 @@ function About() {
         <Col>
           <Sidebar />
         </Col>
-        <Col className="ml-[350px] 2xl:pt-20 text-3xl block">
+        <Col className="ml-[350px] xl:pt-20 text-3xl block">
           <Row className="">
             <h2 className="font-normal text-xl px-5">About</h2>
             <p className="font-light text-gray-400 text-sm px-5 py-2">
               Home / About
             </p>
           </Row>
-          <p className="animate-bounce py-10 bounceIn">Better sleep</p>
-          <p className="animate-bounce flex justify-end pr-20 py-10">
-            Better you
+          <p className="text-sm px-5 flex flex-wrap">
+            Sleep is an essential function that allows your body and mind to
+            recharge, leaving you refreshed and alert when you wake up. Healthy
+            sleep also helps the body remain healthy and stave off diseases.
+            Without enough sleep, the brain cannot function properly. This can
+            impair your abilities to concentrate, think clearly, and process
+            memories.
+            <br /> <br />
+            Most adults require between seven and nine hours of nightly sleep.
+            Children and teenagers need substantially more sleep, particularly
+            if they are younger than five years of age. Work schedules,
+            day-to-day stressors, a disruptive bedroom environment, and medical
+            conditions can all prevent us from receiving enough sleep. A healthy
+            diet and positive lifestyle habits can help ensure an adequate
+            amount of sleep each night – but for some, chronic lack of sleep may
+            be the first sign of a sleep disorder.
           </p>
-          <p className="animate-bounce py-10">Track your sleep cycle</p>
-          <p className="animate-bounce flex justify-end pr-10 py-10">
-            Monitor sleep quality
-          </p>
-          <p className="animate-bounce py-10">Improve your daily life</p>
         </Col>
       </Row>
     </Container>
