684c35c10b80fbcf1de2ce8d4a0d3e5bf68615c1
Valentina Tucak <vtucak@Valentinas-MacBook-Pro.local>
2022-03-03 18:17:48 +0100

    Adding realtime data to graphs

--- src/pages/statistics.jsx
+++ src/pages/statistics.jsx
@@ -4,9 +4,9 @@ import { Col, Container, Row } from "react-bootstrap";
 import Chart from "../components/dashboard/chart";
 import User from "../components/login/user";
 
-function About() {
+function Statistics() {
   return (
-    <main className="flex flex-col items-start h-screen ml-80 pt-6">
+    <Container className="flex flex-col items-start h-screen ml-80 pt-6">
       <Row>
         <Col>
           <Sidebar />
@@ -19,12 +19,14 @@ function About() {
           </div>
         </Col>
         <Col className="ml-[350px] pt-10">
+          <p>Last 5 entries</p>
           <Chart displayMode={"latest"} />
+          <p>Average sleep</p>
           <Chart displayMode={"average"} />
         </Col>
       </Row>
-    </main>
+    </Container>
   );
 }
 
-export default About;
+export default Statistics;
