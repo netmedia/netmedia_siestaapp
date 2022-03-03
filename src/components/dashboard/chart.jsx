6be304f2f39e4f7584e3a668245d03db6332b1d8
Valentina Tucak <vtucak@Valentinas-MacBook-Pro.local>
2022-03-03 17:48:42 +0100

    Adding realtime data to graphs

--- src/components/dashboard/chart.jsx
+++ src/components/dashboard/chart.jsx
@@ -1,10 +1,4 @@
-import {
-  VictoryBar,
-  VictoryLine,
-  VictoryChart,
-  VictoryTheme,
-  VictoryAxis,
-} from "victory";
+import { VictoryBar, VictoryChart } from "victory";
 import { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { getAllEntries } from "../../redux/entries/entriesActions";
@@ -13,6 +7,7 @@ import axios from "axios";
 const Chart = ({ displayMode }) => {
   const dispatch = useDispatch();
   const listOfEntries = useSelector((state) => state.entries.entries);
+  listOfEntries.sort(sortFunction);
 
   useEffect(() => {
     dispatch(
@@ -36,57 +31,111 @@ const Chart = ({ displayMode }) => {
     return hours;
   };
 
-  const latestData = [
-    { day: "1", hours: 5 },
-    { day: "2", hours: 4 },
-    { day: "3", hours: 8 },
-    { day: "4", hours: 6 },
-    { day: "5", hours: 3 },
-    { day: "6", hours: 7 },
-    { day: "7", hours: 10 },
-  ];
+  function sortFunction(a, b) {
+    var dateA = new Date(a.date).getTime();
+    var dateB = new Date(b.date).getTime();
+    return dateA > dateB ? 1 : -1;
+  }
 
-  const averageData = [
-    { week: 1, avgHours: 2 },
-    { week: 2, avgHours: 7 },
-    { week: 3, avgHours: 3 },
-    { week: 4, avgHours: 8 },
-    { week: 5, avgHours: 4 },
-  ];
-
+  const formatDate = (dateString) => {
+    const options = {
+      year: "numeric",
+      month: "numeric",
+      day: "numeric",
+    };
+    return new Date(dateString).toLocaleDateString(options);
+  };
   ////////////////////////////////////////////////
 
   switch (displayMode) {
     case "latest":
       return (
-        <VictoryChart domainPadding={20} theme={VictoryTheme.material}>
-          <VictoryAxis tickValues={[1, 2, 3, 4, 5]} />
-          <VictoryAxis dependentAxis tickFormat={(y) => `${y} h`} />
-          <VictoryBar
-            animate={{
-              duration: 2000,
-              onLoad: { duration: 1000 },
-            }}
-            data={latestData}
-            x="day"
-            y="hours"
-          />
+        <VictoryChart domainPadding={20} width={450}>
+          {listOfEntries
+            .filter((value, key) => key < 6)
+            .map((value, key) => {
+              return (
+                <VictoryBar
+                  key={key}
+                  animate={{
+                    duration: 2000,
+                    onLoad: { duration: 1000 },
+                  }}
+                  data={[
+                    {
+                      x: formatDate(value.date),
+                      y: hoursOfSleep(
+                        parseInt(value.startTime),
+                        parseInt(value.endTime)
+                      ),
+                    },
+                  ]}
+                  style={{
+                    data: { stroke: "#166783", strokeWidth: 7 },
+                  }}
+                />
+              );
+            })}
         </VictoryChart>
       );
+
     case "average":
       return (
-        <VictoryChart theme={VictoryTheme.material}>
-          <VictoryAxis tickValues={[1, 2, 3, 4, 5]} />
-          <VictoryAxis dependentAxis tickFormat={(y) => `${y} h`} />
-          <VictoryLine
-            animate={{
-              duration: 2000,
-              onLoad: { duration: 1000 },
-            }}
-            data={averageData}
-            x="week"
-            y="avgHours"
-          />
+        <VictoryChart domainPadding={20} width={450}>
+          {listOfEntries.map((value, key) => {
+            return (
+              <VictoryBar
+                key={key}
+                animate={{
+                  duration: 2000,
+                  onLoad: { duration: 1000 },
+                }}
+                data={[
+                  {
+                    x: formatDate(value.date).slice(6, 10),
+                    y: hoursOfSleep(
+                      parseInt(value.startTime),
+                      parseInt(value.endTime)
+                    ),
+                  },
+                ]}
+                style={{
+                  data: { stroke: "#166783", strokeWidth: 7 },
+                }}
+              />
+            );
+          })}
+        </VictoryChart>
+      );
+
+    default:
+      return (
+        <VictoryChart domainPadding={20} width={450}>
+          {listOfEntries
+            .filter((value, key) => key < 6)
+            .map((value, key) => {
+              return (
+                <VictoryBar
+                  key={key}
+                  animate={{
+                    duration: 2000,
+                    onLoad: { duration: 1000 },
+                  }}
+                  data={[
+                    {
+                      x: formatDate(value.date),
+                      y: hoursOfSleep(
+                        parseInt(value.startTime),
+                        parseInt(value.endTime)
+                      ),
+                    },
+                  ]}
+                  style={{
+                    data: { stroke: "#166783", strokeWidth: 7 },
+                  }}
+                />
+              );
+            })}
         </VictoryChart>
       );
   }
--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -14,17 +14,19 @@ const Dashboard = () => {
     {
       name: "Alarms",
       count: 10,
-      icon: <BsAlarm className='w-10 h-10 fill-siesta-blue-light' />,
+      icon: <BsAlarm className="w-10 h-10 fill-siesta-blue-light" />,
     },
     {
       name: "Calculator",
       count: 3,
-      icon: <BsFillCalculatorFill className='w-10 h-10 fill-siesta-blue-light' />,
+      icon: (
+        <BsFillCalculatorFill className="w-10 h-10 fill-siesta-blue-light" />
+      ),
     },
     {
       name: "Statistics",
       count: 2,
-      icon: <AiOutlineBarChart className='w-10 h-10 fill-siesta-blue-light' />,
+      icon: <AiOutlineBarChart className="w-10 h-10 fill-siesta-blue-light" />,
     },
   ];
 
@@ -36,27 +38,29 @@ const Dashboard = () => {
 
   return (
     <Container fluid>
-      <Row className='max-w-6xl flex flex-col gap-8 mr-0'>
+      <Row className="max-w-6xl flex flex-col gap-8 mr-0">
         <Col>
           <Sidebar />
         </Col>
-        <Col lg={6} className='ml-[350px] xl:pt-20'>
-          <Row className=''>
-            <h2 className='font-normal text-xl px-5'>Dashboard</h2>
-            <p className='font-light text-gray-400 text-sm px-5 py-2'>Home / Dashboard</p>
+        <Col lg={6} className="ml-[350px] xl:pt-20">
+          <Row className="">
+            <h2 className="font-normal text-xl px-5">Dashboard</h2>
+            <p className="font-light text-gray-400 text-sm px-5 py-2">
+              Home / Dashboard
+            </p>
             <User />
           </Row>
-          <Row className='flex items-center justify-between gap-8 flex-nowrap px-5 py-3'>
+          <Row className="flex items-center justify-between gap-8 flex-nowrap px-5 py-3">
             {widgetContent.map((items, key) => {
               return <UserWidget items={items} key={key} />;
             })}
           </Row>
 
-          <Row className='max-w-6xl px-5 xl:pt-10 flex flex-wrap gap-4 items-start justify-between'>
+          <Row className="max-w-6xl px-5 xl:pt-10 flex flex-wrap gap-4 items-start justify-between">
             <Col>
-              <h2 className='font-normal text-xl'>Sleep Entries</h2>
-              <p className='font-light text-gray-400 text-sm py-2'>Activity</p>
-              <div className='text-xs px-5 py-2 bg-siesta-grey-light rounded-full flex place-items-center justify-between'>
+              <h2 className="font-normal text-xl">Sleep Entries</h2>
+              <p className="font-light text-gray-400 text-sm py-2">Activity</p>
+              <div className="text-xs px-5 py-2 bg-siesta-grey-light rounded-full flex place-items-center justify-between">
                 <p
                   onClick={() => {
                     setDisplayMode("latest");
@@ -67,7 +71,7 @@ const Dashboard = () => {
                       : defaultMarkupClasses.defaultClass
                   }
                 >
-                  Last 7 entries
+                  Last 5 entries
                 </p>
                 <p
                   onClick={() => {
@@ -84,7 +88,7 @@ const Dashboard = () => {
               </div>
               <Chart displayMode={displayMode} />
             </Col>
-            <Col className='flex flex-col w-3/12 pt-24 gap-1'>
+            <Col className="flex flex-col w-3/12 pt-24 gap-1">
               <UpcomingAlarms />
             </Col>
           </Row>
