e5471ae260df7dcfbaa01f0d3bf5311d81dd24a1
rvladusic <rudi.vladusic9@gmail.com>
2022-03-16 10:34:37 +0100

    round 2.4 of refactoring: routing

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,19 +1,18 @@
 import { useHistory } from 'react-router-dom';
 import { useGoogleLogin } from 'react-google-login';
 import { refreshTokenSetup } from '../../utils/refreshToken';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { userLoginFailure, userLoginSuccess } from '../../redux/user/userActions';
 
 const clientId = process.env.REACT_APP_GOOGLE_CLIENT_ID;
 
 const Login = () => {
+  const isUserLogged = useSelector((state) => state.user.credentials.loggedIn);
   let history = useHistory();
   const dispatch = useDispatch();
 
   const onSuccess = (googleData) => {
-    dispatch(
-      userLoginSuccess(googleData.profileObj, `🙋  Welcome ${googleData.profileObj.givenName}`)
-    );
+    dispatch(userLoginSuccess(googleData.profileObj, `🙋  Welcome ${googleData.profileObj.givenName}`));
     refreshTokenSetup(googleData);
     history.push('/dashboard');
   };
@@ -32,7 +31,16 @@ const Login = () => {
 
   return (
     <div>
-      <button onClick={signIn} className=''>
+      <button
+        onClick={() => {
+          if (isUserLogged) {
+            history.push('/dashboard');
+          } else {
+            signIn();
+          }
+        }}
+        className=''
+      >
         <span className='px-2 text-white font-regular text-2xl hover:text-siesta-grey-light hover:border-b-3 hover:border-siesta-grey-light'>
           Login
         </span>
