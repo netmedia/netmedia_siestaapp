3e72556e28da7eb2cdb4b22f0f2dd0ec4860b987
rudivladusic <rudi.vladusic9@gmail.com>
2022-03-23 15:10:09 +0100

    round 3.6 of refactoring: fixing bug on pieChart component

--- src/components/statistics/pieChart.jsx
+++ src/components/statistics/pieChart.jsx
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
 import { Pie } from 'react-chartjs-2';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { format } from 'date-fns';
+import { getAllEntries } from '../../redux/entries/entriesActions';
 
 const PieCharts = ({ displayMode }) => {
   const listOfEntries = useSelector((state) => state.entries.entries);
   listOfEntries.sort(sortFunction);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(
+      getAllEntries(
+        `http://localhost:3005/entries?userId=${
+          JSON.parse(localStorage.getItem('loginData')).googleId
+        }`
+      )
+    );
+
+    //eslint-disable-next-line
+  }, []);
 
   function sortFunction(a, b) {
     var dateA = new Date(a.date).getTime();
@@ -42,10 +56,7 @@ const PieCharts = ({ displayMode }) => {
       {
         label: 'Display',
         data: listOfEntries.map((value) => {
-          return hoursOfSleep(
-            parseInt(value.startTime),
-            parseInt(value.endTime)
-          );
+          return hoursOfSleep(parseInt(value.startTime), parseInt(value.endTime));
         }),
         backgroundColor: listOfEntries.map(() => {
           return String(setBg());
